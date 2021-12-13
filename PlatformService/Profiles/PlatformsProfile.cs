5c39759e7779fc9c03c543e87308f2854ea71465
Steve Russell <steve.russell@swintontechnology.com>
2021-12-13 16:57:35 +0000

    Dto added to be used for publishing a platform message on the RMQ bus

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -12,6 +12,7 @@ namespace PlatformService.Profiles
             // AutoMapper will autimatically map fields of the same name in the source and target objects
             CreateMap<Platform/*Source model*/,PlatformReadDto/*Target Dto*/>();    // this is a one-way mapping. To map both ways you need to explicitly code it
             CreateMap<PlatformCreateDto, Platform>();           // map the creation scenerio from the external dto to the internal representation
+            CreateMap<PlatformReadDto, PlatformPublishedDto>();           // map the creation scenerio from the external dto to the internal representation
         }
     }
 }
